27a17063ff3e38616989528dc4dfa467e200873a
Poncho03 <luis030199@gmail.com>
2023-02-07 20:30:50 -0600

    Avances
    * Se agregan nuevos componentes para nuevas páginas.
    * Se Inicia diseño preliminar de sección "Nosotros"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,23 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CompaniesComponent } from './pages/companies/companies.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CompaniesComponent,
+    PageNotFoundComponent,
+    ProductsComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
