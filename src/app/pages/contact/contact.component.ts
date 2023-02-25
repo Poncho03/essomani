62dce3408a0ba2264c780427a1bbc7cb7381f6b3
Poncho03 <luis030199@gmail.com>
2023-02-25 14:06:48 -0600

    Avances
    * Se retira sección hasta un mejor planteamiento.
    * Se eliminan estilos repetidos.
    * Se agrega diseño preliminar a la sección de "Servicios"
    * Se comienza sección de "Contacto".

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit, AfterViewInit{
 
+  private fragment: string | null | undefined;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.fragment.subscribe(fragment => {
+      this.fragment = fragment;
+    })
+  }
+
+  ngAfterViewInit(): void {
+    try {
+      document.querySelector('#' + this.fragment)?.scrollIntoView();
+    } catch (error) { }
+  }
 }
