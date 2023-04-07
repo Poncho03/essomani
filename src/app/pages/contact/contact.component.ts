a9a5606b6a70e4629b4c40342e0ee5795372dcbe
Poncho03 <luis030199@gmail.com>
2023-04-06 18:06:46 -0600

    Avances
    Correcciones en información de correos electrónicos.
    Se retira iFrame de mapas para una mejor carga, se agrega link de Google Maps en su lugar.

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -9,6 +9,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ContactComponent implements OnInit, AfterViewInit{
 
   private fragment: string | null | undefined;
+  public linkMaps: string = "https://www.google.com.mx/maps/place/Essomani+S.A.+de+C.V./@18.8525401,-97.060752,17z/data=!3m1!4b1!4m6!3m5!1s0x85c4fd3f2bcf3891:0xb07fc0097a357a07!8m2!3d18.8525401!4d-97.058558!16s%2Fg%2F11txpr2xjn";
 
   constructor(private route: ActivatedRoute) { }
 
